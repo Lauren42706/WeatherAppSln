f6f7f548f1196f7b6ff3ca08823bc576444ce105
Lauren42706 <4270683@myuwc.ac.za>
2022-06-08 10:47:51 +0200

    gitIgnore txt fileadded

--- WeatherApp/WeatherApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ WeatherApp/WeatherApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace WeatherApp {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry _cityEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            _cityEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "_cityEntry");
         }
     }
 }
